e6cbf571251ddcc75be3ac357ad899d528599ac0
Allysson2023 <allyssoncarlos.ac21@gmail.com>
2026-04-30 20:56:52 -0300

    Refatorando os botoes de delete, token, login

--- js/app.js
+++ js/app.js
@@ -1,12 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const token = localStorage.getItem("token");
-
-    if(token){
-        liberarSistema();
-    } else {
-        bloquearSistema();
-    }
-});
+// 🔓 FUNÇÕES GLOBAIS (FORA DO DOMContentLoaded)
 
 function liberarSistema(){
     document.getElementById("tela-login").style.display = "none";
@@ -19,3 +11,62 @@ function bloquearSistema(){
     document.getElementById("tela-login").style.display = "flex";
     document.getElementById("sistema").style.display = "none";
 }
+
+
+// 🚀 INÍCIO DO SITE
+document.addEventListener("DOMContentLoaded", () => {
+
+    const token = localStorage.getItem("token");
+
+    if(token){
+        liberarSistema();
+    } else {
+        bloquearSistema();
+    }
+
+    // FORM
+    const form = document.getElementById("form-produto");
+
+    if(!form){
+        console.log("Form NÃO encontrado ❌");
+        return;
+    }
+
+    console.log("Form OK ✅");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        console.log("CLICOU 🔥");
+
+        const nome = document.getElementById("nome").value;
+        const preco = document.getElementById("preco").value;
+        const imagem = document.getElementById("imagem").files[0];
+
+        console.log("DADOS:", nome, preco, imagem);
+
+        const formData = new FormData();
+        formData.append("nome", nome);
+        formData.append("preco", preco);
+        formData.append("imagem", imagem);
+
+        fetch("http://localhost:3000/upload-produto", {
+            method: "POST",
+            headers: {
+                ...getAuthHeaders()
+            },
+            body: formData
+        })
+        .then(res => res.json())
+        .then(data => {
+            alert("Produto cadastrado!");
+            form.reset();
+            carregarProdutos();
+        })
+        .catch(err => {
+            console.log("ERRO:", err);
+            alert("Erro ao cadastrar!");
+        });
+    });
+
+});
--- js/produtos.js
+++ js/produtos.js
@@ -15,6 +15,7 @@ function carregarProdutos(){
 
                 <button onclick="deletarProduto(${produto.id})">Deletar</button>
                 <button onclick='editarProduto(${produto.id}, "${produto.nome}", ${produto.preco})'>Editar</button>
+                <button onclick="editarImagem(${produto.id})">Trocar Imagem </button>
             `;
 
             container.appendChild(div);
@@ -29,5 +30,86 @@ function deletarProduto(id){
     .then(() => {
         alert("Deletado!");
         carregarProdutos();
+    });
+}
+
+function editarImagem(id) {
+    const input = document.createElement("input");
+    input.type = "file";
+
+    input.onchange = () => {
+        const arquivo = input.files[0];
+
+        if(!arquivo) return;
+
+        const formData = new FormData();
+        formData.append("imagem", arquivo);
+
+        fetch(`http://localhost:3000/produtos/${id}/imagem`, {
+            method:"PUT",
+            headers: {
+            ...getAuthHeaders()
+        },
+        body: formData
+        })
+        .then(res => {
+            console.log("resposta:", res);
+            return res.json();
+            })
+            .then(data => {
+                console.log("dados: ", data);
+                
+            alert("Imagem atualizada!");
+            location.reload();
+        })
+        .catch(err => console.log("Erro real: ", err));
+    };
+    input.click();
+}
+
+function editarProduto(id, nome, preco){
+    const token = localStorage.getItem("token");
+
+    if(!token){
+        alert("Você precisa estar logado!");
+        return;
+    }
+
+    const novoNome = prompt("Novo nome: ", nome);
+    const novoPreco = parseFloat(prompt("Novo preço: ", preco));
+
+    if(!novoNome || isNaN(novoPreco)) {
+        alert("Dados inválidos!");
+        return;
+    }
+
+    fetch(`http://localhost:3000/produtos/${id}`, {
+        method: "PUT",
+        headers: {
+            ...getAuthHeaders(),
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: novoNome,
+            preco: novoPreco
+        })
+    })
+    .then(res => {
+        console.log("STATUS PUT:", res.status);
+
+        if(!res.ok){
+            throw new Error("Erro na requisição");
+        }
+
+        return res.json();
+    })
+    .then(data => {
+        console.log("RESPOSTA:", data);
+        alert("Produto atualizado!");
+        location.reload();
+    })
+    .catch(err => {
+        console.error("Erro PUT:", err);
+        alert("Erro ao atualizar produto!");
     });
 }
