6a5d08f7fd9b592e384b5786bed63a172dbaff6d
Allysson2023 <allyssoncarlos.ac21@gmail.com>
2026-05-03 22:41:43 -0300

    Refaturando o token, quando o token é expirado automaticamente sair do login do admin

--- js/api.js
+++ js/api.js
@@ -7,5 +7,25 @@ function apiFetch(url, options = {}){
             ...getAuthHeaders(),
             ...(options.headers || {})
         }
-    });
+    })
+    .then(res => {
+        if (res.status === 401) {
+            localStorage.removeItem("token");
+            localStorage.removeItem("isAdmin");
+
+            mostrarToast("Sessão expirada! Faça login novamente.");
+
+            const adminArea = document.querySelector(".admin-actions");
+            if(adminArea) {
+                adminArea.style.display = "none";
+            }
+
+            setTimeout(() => {
+                location.reload();
+            }, 1500);
+
+            throw new Error("Token expirado");
+        }
+        return res;
+    })
 }
